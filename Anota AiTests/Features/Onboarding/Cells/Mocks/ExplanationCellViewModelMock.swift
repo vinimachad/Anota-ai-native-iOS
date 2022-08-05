5e157dd83d24f33aafeed56c5d9b5c2398b7431f
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-08-05 11:52:34 -0400

    ExplanationCellSnapshot snapshots

---
+++ Anota AiTests/Features/Onboarding/Cells/Mocks/ExplanationCellViewModelMock.swift
@@ -0,0 +1,16 @@
+//
+//  ExplanationCellViewModelMock.swift
+//  Anota AiTests
+//
+//  Created by Vinicius Galhardo Machado on 05/08/22.
+//
+
+import Foundation
+@testable import Anota_Ai
+import UIKit
+
+class ExplanationCellViewModelMock: ExplanationCellViewModelProtocol {
+    var title: String?
+    var body: String?
+    var cover: UIImage?
+}
