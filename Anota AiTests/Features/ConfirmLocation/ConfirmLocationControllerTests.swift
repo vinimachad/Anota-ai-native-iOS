701be7e5506fbffd1db1d0caa3c20a600ce31bc0
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-08-13 13:57:44 -0400

    confirm location snapshot tests

---
+++ Anota AiTests/Features/ConfirmLocation/ConfirmLocationControllerTests.swift
@@ -0,0 +1,8 @@
+//
+//  ConfirmLocationControllerTests.swift
+//  Anota AiTests
+//
+//  Created by Vinicius Galhardo Machado on 13/08/22.
+//
+
+import Foundation
---
+++ Anota AiTests/Features/ConfirmLocation/ConfirmLocationViewModelTests.swift
@@ -0,0 +1,8 @@
+//
+//  ConfirmLocationViewModelTests.swift
+//  Anota AiTests
+//
+//  Created by Vinicius Galhardo Machado on 13/08/22.
+//
+
+import Foundation
