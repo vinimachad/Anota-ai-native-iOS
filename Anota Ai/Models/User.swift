adb01ea6cccde932aa36f2bd0de3fbc9ecd8b79c
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-08-17 16:37:21 -0400

    Create user session

---
+++ Anota Ai/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  Anota Ai
+//
+//  Created by Vinicius Galhardo Machado on 17/08/22.
+//
+
+import Foundation
+
+class User: Codable {
+    
+}
---
+++ Anota Ai/Support/Session/UserSession.swift
@@ -0,0 +1,15 @@
+//
+//  UserSession.swift
+//  Anota Ai
+//
+//  Created by Vinicius Galhardo Machado on 17/08/22.
+//
+
+import Foundation
+
+struct UserSession: Codable {
+    var id: String
+    var email: String
+    var name: String
+    var refreshToken: String
+}
